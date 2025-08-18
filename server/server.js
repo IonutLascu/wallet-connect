522dc00acb987cbc3d0e1251aedd83a5cdb5db3b
Ionut Lascu <lascu.ionut95@gmail.com>
2025-08-18 12:38:05 +0300

    fix: cors origins react app

--- server/server.js
+++ server/server.js
@@ -7,7 +7,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // ---- DB ----
